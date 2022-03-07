900caf2f3bfe9ad46ee802f43911b1906a7ba34d
21bca54 <96609681+21bca54@users.noreply.github.com>
2022-03-07 20:28:00 +0530

    Add files via upload

---
+++ Solution/Solution 5 (1).c
@@ -0,0 +1,37 @@
+/*wap to enter 5 string and print it*/
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char s[5][10]; //s Contain 5 string and each string has 9 Character
+    int i,v,j;
+    clrscr();
+    //logic :to Input 5 string
+    for(i=0; i<5; i++)
+    {
+        printf("Enter name %d:",i+1);
+
+        gets(s[i]);
+    }
+// Logic:  TO how many vovels in string.
+    printf("\n========================\n");
+
+    for(i=0; i<5; i++)
+    {
+        printf("Vovel in string .");
+        for(j=0; j<5; j++)
+        {
+               
+          
+            if(s[i][j]=='A'||s[i][j]=='E'||s[i][j]=='I'||s[i][j]=='O'||s[i][j]=='U'||s[i][j]=='a'||s[i][j]=='e'||s[i][j]=='i'||s[i][j]=='o'||s[i][j]=='u')
+            { 
+               v++;
+               printf("%d,",v);
+               printf("%c,",s[i][j]);
+                 
+            }
+             
+        }printf("\n");
+    }
+    getch();
+}
